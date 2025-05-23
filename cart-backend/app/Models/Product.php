d1216be200964c7797c3453d66419292a15691fe
Amirul <amirulal73@gmail.com>
2025-05-23 20:25:52 +0800

    Added product seeder,model,migration and controller

---
+++ cart-backend/app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'product_name',
+        'product_desc',
+        'product_price',
+        'product_image',
+    ];
+}
